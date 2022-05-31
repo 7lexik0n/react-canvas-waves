ba31c3a44a8277e4c10efb0442397dddd86d8965
LeXik0N <lexik0n1993@yandex.ru>
2022-05-31 22:38:37 +0300

    Added bg settings & accodion elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { Paper, Slider, Typography } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Slider,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import Canvas from "./components/Canvas";
 
@@ -18,9 +27,15 @@ const App = () => {
   const [vaweColor, setVaweColor] = useState(0);
   const [vaweSaturation, setVaweSaturation] = useState(50);
   const [vaweLightness, setVaweLightness] = useState(50);
+  const [redBg, setRedBg] = useState(0);
+  const [greenBg, setGreenBg] = useState(0);
+  const [blueBg, setBlueBg] = useState(0);
+  const [opacityBg, setOpacityBg] = useState(10);
 
   const draw = (ctx, frameCount) => {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillStyle = `rgba(${redBg}, ${greenBg}, ${blueBg}, ${
+      opacityBg * 0.001
+    })`;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.beginPath();
     ctx.moveTo(0, ctx.canvas.height / 2);
@@ -45,7 +60,7 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <div className="app__container">
         <Canvas draw={draw} id="waves" />
-        <Paper
+        <Box
           className="app__sliders"
           sx={{
             position: "absolute",
@@ -56,62 +71,161 @@ const App = () => {
             textAlign: "center",
           }}
         >
-          <Typography component="h5">Амплитуда</Typography>
-          <Slider
-            value={amplitude}
-            onChange={(evt) => setAmplitude(evt.target.value)}
-            min={50}
-            max={350}
-          />
-          <Typography component="h5">Вертикальная скорость</Typography>
-          <Slider
-            value={amplitudeVelociy}
-            scale={(value) => {
-              return 5 * value;
-            }}
-            onChange={(evt, newValue) => {
-              setAmplitudeVelociy(newValue);
-            }}
-            step={1}
-            min={1}
-            max={50}
-          />
-          <Typography component="h5">Горизонтальная скорость</Typography>
-          <Slider
-            value={horizontalSpeed}
-            onChange={(evt) => setHorizontalSpeed(evt.target.value)}
-            min={1}
-            max={15}
-          />
-          <Typography component="h5">Частота</Typography>
-          <Slider
-            value={frequency}
-            onChange={(evt) => setFrequency(evt.target.value)}
-            min={1}
-            max={15}
-          />
-          <Typography component="h5">Цвет волны</Typography>
-          <Slider
-            value={vaweColor}
-            onChange={(evt) => setVaweColor(evt.target.value)}
-            min={0}
-            max={255}
-          />
-          <Typography component="h5">Насыщенность волны</Typography>
-          <Slider
-            value={vaweSaturation}
-            onChange={(evt) => setVaweSaturation(evt.target.value)}
-            min={0}
-            max={100}
-          />
-          <Typography component="h5">Яркость волны</Typography>
-          <Slider
-            value={vaweLightness}
-            onChange={(evt) => setVaweLightness(evt.target.value)}
-            min={0}
-            max={100}
-          />
-        </Paper>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography component="h5" className="app__sliderTitle">
+                Параметры волны
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography component="h5" className="app__sliderTitle">
+                Амплитуда
+              </Typography>
+              <Slider
+                value={amplitude}
+                onChange={(evt) => setAmplitude(evt.target.value)}
+                min={50}
+                max={350}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Вертикальная скорость
+              </Typography>
+              <Slider
+                value={amplitudeVelociy}
+                scale={(value) => {
+                  return 5 * value;
+                }}
+                onChange={(evt, newValue) => {
+                  setAmplitudeVelociy(newValue);
+                }}
+                step={1}
+                min={1}
+                max={50}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Горизонтальная скорость
+              </Typography>
+              <Slider
+                value={horizontalSpeed}
+                onChange={(evt) => setHorizontalSpeed(evt.target.value)}
+                min={1}
+                max={15}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Частота
+              </Typography>
+              <Slider
+                value={frequency}
+                onChange={(evt) => setFrequency(evt.target.value)}
+                min={1}
+                max={15}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography component="h5" className="app__sliderTitle">
+                Цвета волны
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography component="h5" className="app__sliderTitle">
+                Цвет волны
+              </Typography>
+              <Slider
+                value={vaweColor}
+                onChange={(evt) => setVaweColor(evt.target.value)}
+                min={0}
+                max={255}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Насыщенность волны
+              </Typography>
+              <Slider
+                value={vaweSaturation}
+                onChange={(evt) => setVaweSaturation(evt.target.value)}
+                min={0}
+                max={100}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Яркость волны
+              </Typography>
+              <Slider
+                value={vaweLightness}
+                onChange={(evt) => setVaweLightness(evt.target.value)}
+                min={0}
+                max={100}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography component="h5" className="app__sliderTitle">
+                Фон
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography component="h5" className="app__sliderTitle">
+                Красный
+              </Typography>
+              <Slider
+                value={redBg}
+                onChange={(evt) => setRedBg(evt.target.value)}
+                min={0}
+                max={255}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Зелёный
+              </Typography>
+              <Slider
+                value={greenBg}
+                onChange={(evt) => setGreenBg(evt.target.value)}
+                min={0}
+                max={255}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Синий
+              </Typography>
+              <Slider
+                value={blueBg}
+                onChange={(evt) => setBlueBg(evt.target.value)}
+                min={0}
+                max={255}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+              <Typography component="h5" className="app__sliderTitle">
+                Прозрачность
+              </Typography>
+              <Slider
+                value={opacityBg}
+                onChange={(evt) => setOpacityBg(evt.target.value)}
+                min={0}
+                max={100}
+                valueLabelDisplay="auto"
+                size="small"
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Box>
       </div>
     </ThemeProvider>
   );
